56f32a522207a1c69c9f8cd945c8c7abe876a95c
Space7Panda <space7panda@gmail.com>
2022-08-06 21:16:42 +0300

    init

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let server = Server::new("127.0.0.1:8080");
+    server.run();
+}
+
+struct Server {
+    address: String,
+}
+
+impl Server {
+    fn new(address: String) -> Self {
+        Self {
+            address,
+        }
+    }
+
+    fn run(self) {
+        
+    }
+}
